73f8cefe9569356602cf83d8e21853f6a421fabf
wraith <solid_haburex@outlook.com>
2022-10-13 05:13:48 -0400

    Made progress on a few random things... cuz I cant sleep

--- python/portscanner/tcp_scan.py
+++ python/portscanner/tcp_scan.py
@@ -2,46 +2,66 @@
 import argparse
 import threading
 import socket
+import sys 
 import os 
 import re
 
+# TODO:     
+# Add Threading
+# Find a way to simplify parse_ports
+# Add basic attempt to banner grab? 
         
     
-class C():
+class Color:
     pre = '\033'
     clear = pre + '[1;0m'
     green = pre + '[1;92m'
     red = pre + '[1;91m'
     
+
 def cprint(msg):
     if msg.startswith('[+]'):
-        print(C.green + msg[:3] + C.clear + msg[3:])
+        print(Color.green + msg[:3] + Color.clear + msg[3:])
     elif msg.startswith('[-]'):
-        print(C.red + msg[:3] + C.clear + msg[3:])
+        print(Color.red + msg[:3] + Color.clear + msg[3:])
     else:
         print(msg)
-    
-    
 
-def main(ips, port):
+
+def _parse_ports(ports: str) -> list[int]:
+    parsed_ports = []
+    ports = re.split(r',', ports)
+    for p in ports:
+        if '-' in p:
+            tmp = re.split(r'-', p)
+            tmp = [ int(_) for _ in  tmp ]
+            if len(tmp) > 2: raise argparse.ArgumentTypeError(f'{p} not a valid argument')
+            for _ in range(tmp[0], tmp[1]+1):
+                parsed_ports.append(int(_))
+        else:
+            parsed_ports.append(int(p))
+    return parsed_ports 
+
+
+def main(ips, ports):
     for ip in ips:
-        #for port in ports:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            resp = s.connect_ex((ip, port))
-            s.close()
-            if resp == 0:
-                cprint(f'[+] {ip}:{port} is open.')
-            else:
-                cprint(f'[-] {ip}:{port} is closed.')
-        except exception as e :
-            cprint(f'[-] Unhandled socket error:\n {e}')
-    return
+        for port in ports:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                resp = s.connect_ex((ip, port))
+                s.close()
+                if resp == 0:
+                    cprint(f'[+] {ip}:{port} is open.')
+                else:
+                    cprint(f'[-] {ip}:{port} is closed.')
+            except exception as e :
+                cprint(f'[-] Unhandled socket error:\n {e}')
+        return
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Port scanner') 
-    parser.add_argument('-p', '--ports', metavar='PORTS', type=int, required=True, help='TODO: Take start-end, port,port,port or -p port -p port' )
+    parser.add_argument('-p', '--ports', metavar='PORTS', type=_parse_ports)
     parser.add_argument('ips', metavar='IPS', nargs='+', type=str)
     args = parser.parse_args()
     main(args.ips, args.ports)
---
+++ python/py-hash/hash.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import hashlib
+import argparse
+'''
+Simple attemtp at making a multipurpose hash script in Python. Just got curious.
+'''
+
+
+def main(algorithims, paths):
+    allowed_algos = [ 'md5', 'sha1', 'sha256', 'sha512' ]
+
+    for path in paths:
+        if not path.is_file(): 
+            print(f'[-] {path} not a file!')
+        else: 
+            for a in algorithims: 
+                if a not in allowed_algos:
+                    cprint(f'[-] {_} not a known algorithim.')
+                else:
+                    alg = hashlib.new(a)
+                    alg.update(path.read_bytes())
+                    print(f'{alg.hexdigest()}\t{path}')
+    return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', metavar='ALGORITHIM', required=True, type=lambda lst: [i for i in lst.split(',')] )
+    parser.add_argument('paths', metavar='PATHS', nargs='+', type=Path)
+    args = parser.parse_args()
+    main(args.a, args.paths)
